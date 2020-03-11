3275e5e110e318b5d6c500a35cc944086ce16727
Relentless <damnrelentless.yt@gmail.com>
2020-03-11 22:00:26 +0100

    Added project files
    Signed-off-by: DAmNRelentless <damnrelentless.yt@gmail.com>

---
+++ src/main/java/rlnt/joincommands/JoinCommands.java
@@ -0,0 +1,17 @@
+package rlnt.joincommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JoinCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
